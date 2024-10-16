93b1f3cb47f8e0831ca46316314fe6a57ce72c10
Dane Rieber <danerieber@gmail.com>
2024-10-16 14:22:20 -0500

    fix merging linux options in full.nix

--- features/full.nix
+++ features/full.nix
@@ -3,23 +3,25 @@
 let
   inherit (pkgs) stdenv;
 in
-{
-  home.packages = with pkgs; [
-    dua
-    ffmpeg-full
-  ];
+lib.mkMerge [
+  {
+    home.packages = with pkgs; [
+      dua
+      ffmpeg-full
+    ];
 
-  programs.mpv.enable = true;
-  programs.yt-dlp.enable = true;
-}
-// lib.mkIf stdenv.isLinux {
-  home.packages = with pkgs; [
-    mumble
-    prismlauncher
-    signal-desktop
-    vesktop
-  ];
+    programs.mpv.enable = true;
+    programs.yt-dlp.enable = true;
+  }
+  (lib.mkIf stdenv.isLinux {
+    home.packages = with pkgs; [
+      mumble
+      prismlauncher
+      signal-desktop
+      vesktop
+    ];
 
-  programs.firefox.enable = true;
-  programs.obs-studio.enable = true;
-}
+    programs.firefox.enable = true;
+    programs.obs-studio.enable = true;
+  })
+]
