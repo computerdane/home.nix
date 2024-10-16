bcdd7ff96a590399f6db65ec39a8300a6a8a56fa
dane <danerieber@gmail.com>
2024-10-15 22:22:24 -0500

    add linux-only programs

--- features/full.nix
+++ features/full.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  inherit (pkgs) stdenv;
+in
 {
   home.packages = with pkgs; [
     dua
@@ -8,4 +11,15 @@
 
   programs.mpv.enable = true;
   programs.yt-dlp.enable = true;
+}
+// lib.mkIf stdenv.isLinux {
+  home.packages = with pkgs; [
+    mumble
+    prismlauncher
+    signal-desktop
+    vesktop
+  ];
+
+  programs.firefox.enable = true;
+  programs.obs-studio.enable = true;
 }
