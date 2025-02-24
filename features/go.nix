e89526548e5134d19331db0cb610a9fd63684a67
dane <danerieber@gmail.com>
2025-02-24 14:18:02 -0600

    set up helix for editing .proto files

--- features/go.nix
+++ features/go.nix
@@ -2,9 +2,22 @@
 
 {
   home.packages = with pkgs; [
-    buf
+    clang-tools # needed for protols
     go
     gopls
     protobuf
+    protols
   ];
+
+  programs.helix = {
+    languages.language-server.protols.command = "protols";
+    languages.language = [
+      {
+        name = "protobuf";
+        formatter.command = "protols";
+        auto-format = true;
+        language-servers = [ "protols" ];
+      }
+    ];
+  };
 }
