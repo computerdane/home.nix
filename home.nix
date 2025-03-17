d432154bf5a12c0fe355f07b794b0f1a6edd6235
dane <danerieber@gmail.com>
2025-03-17 18:39:15 -0500

    clean up fish config

--- home.nix
+++ home.nix
@@ -77,44 +77,37 @@ in
     icons = "auto";
   };
 
-  programs.fish = {
-    enable = true;
-    shellInit = lib.mkMerge [
-      ''
+  programs.fish = lib.mkMerge [
+    {
+      enable = true;
+      shellInit = ''
         set fish_greeting
         if test -e ~/.openai-api-key
           export OPENAI_API_KEY=$(cat ~/.openai-api-key)
         end
-      ''
-      (lib.mkIf stdenv.isLinux (
-        lib.concatStringsSep "\n" (
-          lib.map
-            (cmd: ''
-              complete -w journalctl ${cmd}
-              complete -w "systemctl status" ${cmd}
-            '')
-            [
-              "logs"
-              "flogs"
-            ]
-        )
-      ))
-    ];
-    shellAliases = lib.mkMerge [
-      {
+      '';
+      shellAliases = {
         bop = "nix run github:computerdane/bop-bun --";
         cat = "bat";
         gpt = "sgpt";
         my-tide-configure = "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Few icons' --transient=No";
-      }
-      (lib.mkIf stdenv.isLinux {
+      };
+    }
+    (lib.mkIf stdenv.isLinux {
+      shellInit = ''
+        complete -w journalctl logs
+        complete -w "systemctl status" logs
+        complete -w journalctl flogs
+        complete -w "systemctl status" flogs
+      '';
+      shellAliases = {
         logs = "journalctl --no-hostname -aeu";
         flogs = "journalctl --no-hostname -afu";
         rivals-kill-switch = "pkill -9 Xwayland && XAUTHORITY=/run/user/1000/xauth* DISPLAY=:0 steam steam://rungameid/2767030";
         vpn = "sudo ip netns exec pvpn";
-      })
-    ];
-  };
+      };
+    })
+  ];
 
   programs.git = {
     enable = true;
